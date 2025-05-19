6b1aa9182e119dc4c994d693440d01ac851efa74
KateMMiller <kathryn_miller@nps.gov>
2025-05-19 14:18:32 -0400

    wip import multiple zip files

--- R/importData.R
+++ R/importData.R
@@ -1,20 +1,19 @@
 #' @title importData
 #'
-#' @description Imports data from FFI SQL Server database or csvs of FFI database tables using the schmema designed for
+#' @description Imports data from FFI SQL Server database or csvs of FFI database tables using the schema designed for
 #' the Northern Great Plains Network plant community monitoring protocol. Currently can only import from a local installation
 #' of a park FFI database in SQL Server Management Studio (SSMS), but the goal is to add an option for importing directly
 #' from the SQL Server where the production FFI databases are housed. If multiple parks worth of data are imported, tables
 #' are row binded, so that all NPGN parks can be queried, summarized, etc. at once.
 #'
 #' @importFrom dplyr bind_rows collect mutate rename tbl
-#' @importFrom purrr flatten map
+#' @importFrom purrr flatten map set_names
 #'
 #' @param type Indicate how to import the database tables
 #' \describe{
 #' \item{"local"}{Import tables in 'dbo' schema from the local installation of an FFI database in SQL Server Management Studio (SSMS).}
 #' \item{"server"}{Import tables in 'dbo' schema from the FFI database on the production SQL Server (not enabled).}
-#' \item{"csv"}{Import csvs that were exported from the FFI database. Option doesn't require SSMS to be installed (not yet enabled).}
-#' \item{"zip"}{ Import zip file containing csvs that were exported from the FFI database. Option doesn't require SSMS to be installed (not yet enabled).}
+#' \item{"csv"}{ Import zip file containing csvs that were exported from the FFI database. Option doesn't require SSMS to be installed (not yet enabled).}
 #' }
 #'
 #' @param server If type = 'server', requires quoted FFI server address (not currently enabled).
@@ -24,15 +23,15 @@
 #' indicating the dbname in each table. Note that the tables being row binded must be identical for this to work, and there
 #' aren't thorough checks built in the function to ensure that's true (i.e., it's likely to fail, but the error message may be weird).
 #'
-#' @param path If type = "csv" or "zip", specify path of stores
-#'
 #' @param new_env Logical. If TRUE (default), will import tables to VIEWS_NGPN environment. If FALSE, will import tables to global
 #' environment.
 #'
-#' @param export Logical. If TRUE, will export a zip file of csvs to specified path.
+#' @param export Logical. If TRUE, will export a zip file of csvs to specified export_path.
 #'
-#' @param path Quoted string to save zipped csvs to if export = TRUE. If not specified, will export to the working directory.
-#'  Default is export = FALSE.
+#' @param export_path Quoted string to export zipped csvs to if export = TRUE. If not specified, will export to the working directory.
+#'
+#' @param import_path Quoted string to import a zipped file of csvs if type = 'csv'. The name of the zipped file should be included
+#' in the path. Can specify multiple paths to import multiple parks/projects.
 #'
 #' @examples
 #' \dontrun{
@@ -40,7 +39,7 @@
 #' # Import data for AGFO
 #' importData(type = 'local', dbname = c("FFI_RA_AGFO"))
 #'
-#' # Import data for all NGPN parks (takes a few seconds)
+#' # Import data for all NGPN parks (takes a few seconds) from local copy on SSMS
 #' importData(type = 'local',
 #'            dbname = c("FFI_RA_AGFO", "FFI_RA_BADL", "FFI_RA_DETO", "FFI_RA_FOLA",
 #'                       "FFI_RA_FOUS", "FFI_RA_JECA", "FFI_RA_KNRI", "FFI_RA_MNRR",
@@ -54,7 +53,8 @@
 #'
 #' @export
 
-importData <- function(type = "local", server = NA, dbname = "FFI_RA_AGFO", new_env = T, export = F, path = NA){
+importData <- function(type = "local", server = NA, dbname = "FFI_RA_AGFO", new_env = T, export = F,
+                       export_path = NA, import_path = NA){
   #---- Bug Handling ----
   # Check that suggested package required for this function are installed
   if(!requireNamespace("odbc", quietly = TRUE)){
@@ -65,25 +65,39 @@ importData <- function(type = "local", server = NA, dbname = "FFI_RA_AGFO", new_
     stop("Package 'dbplyr' needed for this function to work. Please install it.", call. = FALSE)}
   if(!requireNamespace("zip", quietly = TRUE) & export == T){
     stop("Package 'zip' needed when export = TRUE. Please install it.", call. = FALSE)}
-  type <- match.arg(type, c("local", "server", "csv", 'zip'))
+  type <- match.arg(type, c("local", "server", 'zip'))
   stopifnot(is.logical(new_env))
   stopifnot(is.logical(export))
   if(any(type %in% c("local", "server")) & any(is.na(dbname))){stop("Must specify a dbname if type is 'local' or 'server'")}
   if(type == "server" & is.na(server)){stop("Must specify a server address if type = 'server'")}
 
   #++++++ Update as more features are added ++++++
-  if(type %in% c("server", "csv", "zip")){stop(paste0("Sorry, type = ", type, " is not yet enabled."))}
+  if(type %in% c("server")){stop(paste0("Sorry, type = ", type, " is not yet enabled."))}
 
-  # Error handling for path
+  # Error handling for paths
   if(export == TRUE){
-    if(is.na(path)){
-      path <- getwd()
-      print(paste0("No path specified. Output saved to working directory: ", getwd()), quote = FALSE)}
-    if(!grepl("/$", path)){path <- paste0(path, "/")} # add / to end of path if doesn't exist
-    if(!dir.exists(path)){stop("Specified directory does not exist.")}
+    if(is.na(export_path)){
+      export_path <- getwd()
+      print(paste0("No export_path specified. Output saved to working directory: ", getwd()), quote = FALSE)}
+    if(!grepl("/$", export_path)){export_path <- paste0(export_path, "/")} # add / to end of path if doesn't exist
+    if(!dir.exists(export_path)){stop("Specified export_path directory does not exist.")}
     # Normalize filepath for zip
-    pathn <- normalizePath(path)
+    export_pathn <- normalizePath(export_path)
     }
+
+  if(type == 'zip'){
+    if(!any(file.exists(import_path))){
+      stop(paste0("Specified import_path does not exist. ",
+                  ifelse(any(grepl("sharepoint", import_path)), " Note that file paths from Sharepoint or Teams are not accessible.",
+                         "")))}
+
+    if(is.na(import_path)){stop("Must specify an import_path for type = 'csv'.")}
+    if(!grepl(".zip$", import_path)){stop("Must include the name of the zip file in import_path.")} # add / to end of path if doesn't exist
+    if(!dir.exists(import_path)){stop("Specified import_path directory does not exist.")}
+    # Normalize filepath for zip
+    import_pathn <- normalizePath(import_path)
+  }
+
 
   if(new_env == TRUE){VIEWS_NGPN <<- new.env()}
   env <- if(new_env == TRUE){VIEWS_NGPN} else {.GlobalEnv}
@@ -138,11 +152,11 @@ importData <- function(type = "local", server = NA, dbname = "FFI_RA_AGFO", new_
     park <- substr(dbname, nchar(dbname)-3, nchar(dbname))
     zip_name = paste0("NGPN_FFI_export_", park, "_", format(Sys.Date(), "%Y%m%d"), ".zip")
 
-    zip::zipr(zipfile = paste0(pathn, "\\", zip_name),
+    zip::zipr(zipfile = paste0(export_pathn, "\\", zip_name),
               root = tmp,
               files = file_list)
     # csvs will be deleted as soon as R session is closed b/c tempfile
-    noquote(paste0('Export complete. Data package saved to: ', pathn, zip_name))
+    noquote(paste0('Export complete. Data package saved to: ', export_pathn, zip_name))
 
   }
 
@@ -204,11 +218,11 @@ importData <- function(type = "local", server = NA, dbname = "FFI_RA_AGFO", new_
 
       zip_name = paste0("NGPN_FFI_export", format(Sys.Date(), "%Y%m%d"), ".zip")
 
-      zip::zipr(zipfile = paste0(pathn, "\\", zip_name),
+      zip::zipr(zipfile = paste0(export_pathn, "\\", zip_name),
                 root = tmp,
                 files = file_list)
       # csvs will be deleted as soon as R session is closed b/c tempfile
-      noquote(paste0('Export complete. Data package saved to: ', pathn, zip_name))
+      noquote(paste0('Export complete. Data package saved to: ', export_pathn, zip_name))
     }
 
   } # end of dbname>1
@@ -217,14 +231,15 @@ importData <- function(type = "local", server = NA, dbname = "FFI_RA_AGFO", new_
   if(type == 'csv'){
 
     # Pulling in only tables commonly used across NGPN parks
-    csv_list <- c('AuxSpecies', 'Cover_Frequency_metric_Attribute', 'Cover_Frequency_metric_Sample',
+    csv_list1 <- c('AuxSpecies', 'Cover_Frequency_metric_Attribute', 'Cover_Frequency_metric_Sample',
                   'Cover_Points_metric_Attribute', 'Cover_Points_metric_Sample', 'Cover_SpeciesComposition_metric_Attribute',
                   'Cover_SpeciesComposition_metric_Sample', 'DataGridViewSettings', 'Density_Belts_metric_Attribute',
                   'Density_Belts_metric_Sample', 'Density_Quadrats_metric_Attribute', 'Density_Quadrats_metric_Sample',
                   'DisturbanceHistory_Attribute', 'DisturbanceHistory_Sample', 'FuelConstants_CWD', 'FuelConstants_DL',
                   'FuelConstants_ExpDL', 'FuelConstants_FWD', 'FuelConstants_Veg', 'LU_Contact', 'LU_DataLevel', 'LU_DataType',
                   'LU_LifeCycle', 'LU_LifeForm', 'LU_MacroPlot_Type', 'LU_Shape', 'LU_Unit', 'Last_Modified_Date', 'LocalSpecies',
-                  'MM_LocalSpecies_SpeciesPickList', 'MM_MonitoringStatus_SampleEvent', 'MM_Organization_Method', 'MM_ProjectUnit_MacroPlot',
+                  'MM_LocalSpecies_SpeciesPickList', 'MM_Method_Reference',
+                  'MM_MonitoringStatus_SampleEvent', 'MM_Organization_Method', 'MM_ProjectUnit_MacroPlot',
                   'MM_Project_Protocol', 'MM_Protocol_Method', 'MM_SampleEvent_Protocol', 'MSchange_tracking_history',
                   'MacroPlot', 'MasterSpecies', 'MasterSpecies_LastModified', 'Method', 'MethodAttribute', 'MethodAttributeCode',
                   'MethodVersion', 'MonitoringStatus', 'Organization', 'OrganizationGroup', 'PostBurnSeverity_metric_Attribute',
@@ -232,14 +247,137 @@ importData <- function(type = "local", server = NA, dbname = "FFI_RA_AGFO", new_
                   'SampleAttribute', 'SampleAttributeCode', 'SampleEvent', 'SchemaVersions', 'Schema_Version', 'Settings',
                   'SpeciesPickList', 'SurfaceFuels_1000Hr_Attribute', 'SurfaceFuels_1000Hr_Sample', 'SurfaceFuels_Duff_Litter_Attribute',
                   'SurfaceFuels_Duff_Litter_Sample', 'SurfaceFuels_Fine_Attribute', 'SurfaceFuels_Fine_Sample',
+                  #"SurfaceFuels_Hr_Attribute", "SurfaceFuels_Hr_Sample",
                   'Trees_Individuals_metric_Attribute', 'Trees_Individuals_metric_Sample')
 
 
+    if(length(import_path) == 1){
+      # Check if can read files within the zip file
+      tryCatch(
+        {zfiles = utils::unzip(import_path, list = T)$Name},
+         error = function(e){stop(paste0("Unable to import specified zip file."))})
+
+      z_list = sort(zfiles[grepl(paste0(csv_list1, collapse = "|"), zfiles)])
+
+      # Drop date stamp (if it exists) from file name if exists in 2 steps
+      z_list_names <- gsub("[[:digit:]]+|.csv", "", z_list)
+      z_list_names <- gsub("./", "", z_list_names)
+      z_list_names <- gsub("_$","", z_list_names)
+
+      # Drop csvs from csv_list not in z_list
+      csv_list <- csv_list1[csv_list1 %in% z_list_names]
+
+      miss_tbls <- setdiff(z_list_names, csv_list) # currently circular. Once I know the tables that should always be included,
+      # I'll update csv_list1 above and use the same tables for each park.
+
+      # Check for missing views
+      if(length(miss_tbls) > 0){stop("Missing the following tables from the specified import_path: ",
+                                     paste0(miss_tbls, collapse = ", "))}
+
+      # Since the missing test passed, clean up files so only includes names in view_list, but
+      # maintain order in files
+
+      # Import views now that all tests passed
+      pb <- txtProgressBar(min = 0, max = length(z_list), style = 3)
+
+      tbls1 <- unzip(import_path, junkpaths = TRUE, exdir = tempdir())
+      tbls2 <- sort(tbls1[grepl(".csv", tbls1)])
+
+      tbls <- sort(tbls2[grepl(paste0(csv_list, collapse = "|"), tbls2)])
+
+      tbl_import <-
+        lapply(seq_along(tbls), function(x){
+          setTxtProgressBar(pb,x)
+          read.csv(tbls[x], na.string = c("NA", "NULL"), check.names = FALSE)})
+
+      tbl_import <- setNames(tbl_import, z_list_names)
+      list2env(tbl_import, envir = env)
+      # Close progress bar
+      close(pb)
+
+    } else if(length(import_path > 1)){
+
+      file_names1 <- sort(sub(".*/", "", import_path, perl = T))
+      file_names2 <- gsub("[[:digit:]]+|.zip", "", file_names1)
+      file_names <- gsub("_$","", file_names2)
+
+      zip_import <-
+      purrr::map(seq_along(import_path), function(ip){
+        tryCatch(
+          {zfiles = utils::unzip(import_path[ip], list = T)$Name},
+           error = function(e){stop(paste0("Unable to import specified zip file: ", ip))})
+
+        z_list = sort(zfiles[grepl(paste0(csv_list1, ".csv", collapse = "|"), zfiles)])
+
+        # Drop date stamp (if it exists) from file name if exists in 2 steps
+        #z_list_names <- gsub("[[:digit:]]+|.csv", "", z_list)
+        z_list_names <- gsub("./", "", z_list)
+        z_list_names <- gsub(".csv$","", z_list_names)
+        z_list_names <- gsub("_$","", z_list_names)
+
+        # Drop csvs from csv_list not in z_list
+        csv_list <- csv_list1[csv_list1 %in% z_list_names]
+
+        # "SurfaceFuels_Hr_Attribute", "SurfaceFuels_Hr_Sample",
+
+        miss_tbls <- setdiff(z_list_names, csv_list) # currently circular. Once I know the tables that should always be included,
+        # I'll update csv_list1 above and use the same tables for each park.
+
+        # Check for missing views
+        if(length(miss_tbls) > 0){stop("Missing the following tables from the specified import_path: ",
+                                       import_path[ip], "\n",
+                                       paste0(miss_tbls, collapse = ", "))}
+
+        # Since the missing test passed, clean up files so only includes names in view_list, but
+        # maintain order in files
+        tbls1 <- unzip(import_path[ip], junkpaths = TRUE, exdir = tempdir())
+        tbls2 <- sort(tbls1[grepl(".csv", tbls1)])
+        tbls <- sort(tbls2[grepl(paste0(csv_list, ".csv", collapse = "|"), tbls2)])
+
+        tbl_import <-
+          lapply(seq_along(tbls), function(x){
+          read.csv(tbls[x], na.string = c("NA", "NULL"), check.names = FALSE)})
+
+        tbl_import <- setNames(tbl_import, z_list_names)},
+        .progress = TRUE) |> purrr::set_names(file_names)
+
+      # flatten list to bind like tables together
+      zipflat1 <- purrr::flatten(zip_import)
+      #+++++ ENDED HERE +++++ Couldn't combine UV1 and UV1 b/c different data types (might have to drop those)
+
+      zipflat2 <- tapply(zipflat1, names(zipflat1), dplyr::bind_rows)
+      # remove empty tables
+      dbflat3 <- dbflat2[sapply(dbflat2, nrow) > 0]
+      # sort tables alphabetically
+      dbflat4 <- dbflat3[sort(names(dbflat3))]
+
+      #VIEWS_NGPN <<- new.env()
+      list2env(dbflat4, envir = env)
+
+      if(export == TRUE){
+        dir.create(tmp <- tempfile())
+        dbtbls <- names(dbflat4)
+
+        invisible(lapply(seq_along(dbtbls), function(x){
+          temp_tbl = get(dbtbls[x], envir = env)
+          write.csv(temp_tbl,
+                    paste0(tmp, "\\", dbtbls[x], ".csv"),
+                    row.names = FALSE)
+        }))
+
+        file_list <- list.files(tmp)
+
+        zip_name = paste0("NGPN_FFI_export", format(Sys.Date(), "%Y%m%d"), ".zip")
+
+        zip::zipr(zipfile = paste0(export_pathn, "\\", zip_name),
+                  root = tmp,
+                  files = file_list)
+        # csvs will be deleted as soon as R session is closed b/c tempfile
+        noquote(paste0('Export complete. Data package saved to: ', export_pathn, zip_name))
+      }
+      } # end of fp 2
   }
 
-  if(type == "zip"){
-
-  }
 
 
 } # end of function
